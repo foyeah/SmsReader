11bb6f657e782eca0f4b60cb55d007f1199490cf
thever4 <Nikita@TheVer4.ru>
2023-10-29 12:30:02 +0500

    Basic android app template

---
+++ app/src/main/java/net/example/smsreader/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.example.smsreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
