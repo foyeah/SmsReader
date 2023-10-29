6568846c5db092d9f08c6729b2e365da1203b97d
thever4 <Nikita@TheVer4.ru>
2023-10-29 13:23:16 +0500

    Installed VBPD by Kirich and clean navigation component library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // vbpd
+    implementation("com.github.kirich1409:viewbindingpropertydelegate-full:1.5.9")
+
+    // Navigation
+    val navigation = "2.6.0"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigation")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigation")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navigation")
+
+
 }
